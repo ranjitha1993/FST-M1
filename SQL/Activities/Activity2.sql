50a963d6ab5960da4895591950b422aa5c2b9da2
ranjitha1993 <ranjitha.n1@ibm.com>
2024-06-28 11:00:47 +0530

    Add files via upload

---
+++ SQL/Activities/Activity2.sql
@@ -0,0 +1,27 @@
+REM   Script: Session m1june-03-activity3
+REM   Practice
+
+
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+
+
+INSERT  INTO salesman VALUES(5005, 'Pit Alex', 'London', 11);
+
+  INSERT  INTO salesman VALUES(5006, 'McLyon', 'Paris', 14);
+
+   INSERT INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13);
+
+   INSERT INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12);
+
+
+
+SELECT * FROM salesman;
+
+--select salesman_id, salesman_city FROM salesman;
+
+--select * from salesman WHERE salesman_city='Paris';
+
+--select salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
