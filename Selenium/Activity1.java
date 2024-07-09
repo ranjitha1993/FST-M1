d2eccbd782c417401df0da475d3da4528b018ded
ranjitha1993 <ranjitha.n1@ibm.com>
2024-07-09 15:45:14 +0530

    Update Activity1.java

--- Selenium/Activity1.java
+++ Selenium/Activity1.java
@@ -1,16 +1,37 @@
-package demos;
-
+package com.example.fst_testng;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Activity1 {
 
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://v1.training-support.net");
-		System.out.println(driver.getTitle());
-		driver.quit();
+public class A1 {
+   
+    WebDriver driver;
+    
+    @BeforeClass
+    public void beforeMethod() {
+  
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();      
+        driver.get("https://alchemy.hguy.co/lms");
+    }
 
-	}
+    @Test
+    public void TestCase1() {
+    	
+        String title = driver.getTitle();       
+        System.out.println("Page title is: " + title);          
+        Assert.assertEquals(driver.getTitle(), "Alchemy LMS – An LMS Application");
+    }
+
+    @AfterClass
+    public void afterMethod() {
+        driver.quit();
+    }
 
 }
