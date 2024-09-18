4295a299254185f0bb7bbeb145fda4c8c46c538a
ranjitha1993 <ranjitha.n1@ibm.com>
2024-09-18 18:02:18 +0530

    Add files via upload

---
+++ JUnit/Activities/NotEnoughFundsException.java
@@ -0,0 +1,9 @@
+package com.example.ibm.fst_junit;
+
+public class NotEnoughFundsException extends RuntimeException {
+
+    public NotEnoughFundsException(Integer amount, Integer balance) {
+        super("Attempted to withdraw " + amount + " with a balance of " + balance);
+    }
+
+}
